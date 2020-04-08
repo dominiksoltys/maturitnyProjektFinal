0a2314725e3e323fd82388b64931340b1dd47117
Dominik <dominosoltys@gmail.com>
2020-04-08 19:56:27 +0200

    zabudnute heslo a design

--- app/src/main/java/com/example/maturitnyprojektfinal/Profil.java
+++ app/src/main/java/com/example/maturitnyprojektfinal/Profil.java
@@ -1,13 +1,22 @@
 package com.example.maturitnyprojektfinal;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -17,10 +26,14 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import javax.annotation.Nullable;
 
 public class Profil extends AppCompatActivity {
+
     TextView fullName,email,phone;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userId;
+    Button resetPassLocal;
+    FirebaseUser user;
+
 
 
 
@@ -36,18 +49,61 @@ public class Profil extends AppCompatActivity {
         fStore = FirebaseFirestore.getInstance();
 
         userId = fAuth.getCurrentUser().getUid();
+        user = fAuth.getCurrentUser();
+
+        resetPassLocal = findViewById(R.id.resetPasswordLocal);
 
         DocumentReference documentReference = fStore.collection("users").document(userId);
         documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
-            phone.setText(documentSnapshot.getString("phone"));
-            fullName.setText(documentSnapshot.getString("fName"));
-            email.setText(documentSnapshot.getString("email"));
+                phone.setText(documentSnapshot.getString("phone"));
+                fullName.setText(documentSnapshot.getString("fName"));
+                email.setText(documentSnapshot.getString("email"));
             }
         });
 
+        resetPassLocal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText resetPassword = new EditText(v.getContext());
+
+                final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Zmena hesla");
+                passwordResetDialog.setMessage("Zadajte vase nove heslo(viac ako 6 znakov)");
+                passwordResetDialog.setView(resetPassword);
+
+                passwordResetDialog.setPositiveButton("Zmenit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //vytiahnutie emailu a odoslanie reset linku
+                        String newPassword = resetPassword.getText().toString();
+                        user.updatePassword(newPassword).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(Profil.this, "Uspesne zmenene heslo", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Profil.this, "Zmena hesla zlyhala", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+
+                passwordResetDialog.setNegativeButton("Zrusit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //zatvorenie dialogu,navrat do Profil.java
+                    }
+                });
+                passwordResetDialog.create().show();
+            }
+        });
     }
+
+
 
     public void goBack(View view) {
         startActivity(new Intent(getApplicationContext(),drawerActivity.class));
