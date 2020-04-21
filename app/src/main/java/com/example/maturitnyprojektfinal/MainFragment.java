08c0ca05f9b020096689d4aaccd1d3df41639789
Dominik <dominosoltys@gmail.com>
2020-04-21 13:58:23 +0200

    dizajn

--- app/src/main/java/com/example/maturitnyprojektfinal/MainFragment.java
+++ app/src/main/java/com/example/maturitnyprojektfinal/MainFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,10 +13,18 @@ import androidx.fragment.app.Fragment;
 
 public class MainFragment extends Fragment {
 
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main,container,false);
         return view;
+
+    }
+    public void posliPostrehy(View view){
+
+
+
+
     }
 }
