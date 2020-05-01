24e8abe478937009a0b4ffdde6670036ddc6034d
Malcovsky, Samuel <malcovsky@spse-po.sk>
2020-05-01 12:20:01 +0200

    Postrehy

--- app/src/main/java/com/example/maturitnyprojektfinal/MainFragment.java
+++ app/src/main/java/com/example/maturitnyprojektfinal/MainFragment.java
@@ -8,24 +8,41 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class MainFragment extends Fragment {
-
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+    FirebaseUser user;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        user = fAuth.getCurrentUser();
+        userId = fAuth.getCurrentUser().getUid();
+
         View view = inflater.inflate(R.layout.fragment_main,container,false);
-        Button odosli = (Button) view.findViewById(R.id.posli);
+        Button odosli = view.findViewById(R.id.posli);
         final EditText editText = view.findViewById(R.id.postrehy);
         odosli.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                Map<String,Object> postrehy = new HashMap<>();
+                postrehy.put("Id", userId);
+                postrehy.put("Text", editText.getText().toString().trim());
+                fStore.collection("postrehy").add(postrehy);
                 editText.setText("");
                 Toast.makeText(getActivity(), "Odoslane", Toast.LENGTH_SHORT).show();
             }
